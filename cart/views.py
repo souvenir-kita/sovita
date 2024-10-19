fa16a28645512d4c18f05b202b5f42bc5913a9f6
MaulRai <muhammadraihanofc@gmail.com>
2024-10-19 23:53:39 +0700

    done tut 1

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def show_cart(request):
+    context = {
+        'npm' : '2306123456',
+        'name': 'Pak Bepe',
+        'class': 'PBP E'
+    }
+
+    return render(request, "main.html", context)
