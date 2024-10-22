b30e7725447d45c6a9bc2e765576aed7b72cfae3
Wisnu Nugroho <47017750+wsnugroho@users.noreply.github.com>
2024-10-22 16:40:53 +0700

    Basic Auth Implementation (No Roles) (#4)
    add basic auth (same as tutorial)

---
+++ authentication/views.py
@@ -0,0 +1,34 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.shortcuts import redirect, render
+
+
+def register(request):
+    form = UserCreationForm()
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Your account has been successfully created!")
+            return redirect("authentication:login")
+    context = {"form": form}
+    return render(request, "register.html", context)
+
+
+def login_user(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("display:display_main")
+    else:
+        form = AuthenticationForm(request)
+    context = {"form": form}
+    return render(request, "login.html", context)
+
+
+def logout_user(request):
+    logout(request)
+    return redirect("authentication:login")
--- souvenir_kita/settings.py
+++ souvenir_kita/settings.py
@@ -10,8 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
-from pathlib import Path
 import os
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,8 +39,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "forum",   # Wisnu
-    "display", # Arzaka
+    "authentication",
+    "forum",  # Wisnu
+    "display",  # Arzaka
     "cart",
 ]
 
