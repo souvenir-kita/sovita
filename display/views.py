78aca9630530988f21d0e34e52b4f6da5bd94268
arzakaraffan <arzakaraffan@gmail.com>
2024-10-23 15:35:44 +0700

    CRUD AJAX Product (Images Applied)

--- display/views.py
+++ display/views.py
@@ -1,24 +1,22 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from display.forms import ProductForm
 from display.models import Product
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+from django.core import serializers
 
 def display_main(request):
     productEntry = Product.objects.all()
     context = {
-        'name' : 'Bolu Bali',
-        'price': 'RP20.000,00',
-        'description' : 'A bolu made in Bali.',
-        'location' : 'Denpasar City, Bali.',
-        'shop': 'Bali Shop',
         'product_entry' : productEntry,
     }
 
     return render(request, "display.html", context)
 
 def create_product(request):
-    form = ProductForm(request.POST or None)
+    form = ProductForm(request.POST or None, request.FILES or None)
 
     if form.is_valid() and request.method == "POST":
         form.save()
@@ -29,6 +27,24 @@ def create_product(request):
                 }
     return render(request, "create_product.html", context)
 
+@csrf_exempt
+@require_POST
+def create_product_ajax(request):
+    name = request.POST.get("name")
+    price = request.POST.get("price")
+    description = request.POST.get("description")
+    picture = request.FILES.get("picture")
+    location = request.POST.get("location")
+    shop = request.POST.get("shop")
+
+    new_product = Product(
+        name = name, price = price, description = description,
+        picture = picture, location = location, shop = shop,
+    )
+    new_product.save()
+
+    return HttpResponse(b"CREATED", status=201)
+
 def view_product(request, id):
     product = get_object_or_404(Product, id=id)
     context = {
@@ -38,7 +54,7 @@ def view_product(request, id):
 
 def edit_product(request, id):
     product = get_object_or_404(Product, id=id)
-    form = ProductForm(request.POST or None, instance=product)
+    form = ProductForm(request.POST or None, request.FILES or None, instance=product)
 
     if request.method == "POST":
         if form.is_valid():
@@ -58,3 +74,19 @@ def delete_product(request, id):
                 'product' : product
     }
     return render(request, "delete_confirmation.html" ,context)
+
+def show_xml(request):
+    data = Product.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = Product.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = Product.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json_by_id(request, id):
+    data = Product.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
