4e320b65d27a4fade3a8d84b2aa8ca19cbecb381
rafliesa <muhammadrafli1579@gmail.com>
2024-10-24 23:24:38 +0700

    mengatur layout home di app promo

--- promo/models.py
+++ promo/models.py
@@ -6,7 +6,7 @@ import uuid
 class Promo(models.Model) :
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     kode = models.CharField(unique= True, max_length= 15, validators=[MinLengthValidator(4)])
-    nama = models.CharField(max_length=100)
+    nama = models.CharField(max_length=20)
     potongan = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0.0)])
     stock = models.IntegerField(validators=[MinValueValidator(0)])
     deskripsi = models.TextField()
