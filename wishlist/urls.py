152a08cc4cfaffe3c317bb0be2dd63bf958f4079
reyhanardian16 <reyhanardian16@gmail.com>
2024-10-26 13:32:05 +0700

    Add toggle wishlist

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from .views import *
 
-appname = "wishlist"
+app_name = 'wishlist'
 
-URLPATTERNS = [
-    path('', wishlist_view, name='show_wishlist'),
-    path('wishlist/toggle/<int:product_id>/', toggle_wishlist, name='toggle_wishlist'),
+urlpatterns = [
+    path('', show_wishlist, name='show_wishlist'),
+    path('toggle/<uuid:product_id>/', toggle_wishlist, name='toggle_wishlist'),
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
@@ -5,19 +6,21 @@ from .models import Wishlist
 from display.models import Product
 
 @login_required
+def show_wishlist(request):
+    wishlist_items = Wishlist.objects.filter(user=request.user).select_related('product')
+    return render(request, 'wishlist.html', {'wishlist_items': wishlist_items})
+
+@login_required
 def toggle_wishlist(request, product_id):
     product = get_object_or_404(Product, id=product_id)
     wishlist_item, created = Wishlist.objects.get_or_create(user=request.user, product=product)
     
     if created:
-        messages.success(request, f"{product.name} was added to your wishlist.")
+        # Product was added to the wishlist
+        action = 'added'
     else:
+        # Product was already in the wishlist, so it was removed
         wishlist_item.delete()
-        messages.success(request, f"{product.name} was removed from your wishlist.")
+        action = 'removed'
     
-    return redirect('product_detail', product_id=product_id)
-
-@login_required
-def wishlist_view(request):
-    wishlist_items = Wishlist.objects.filter(user=request.user).select_related('product')
-    return render(request, 'wishlist/wishlist.html', {'wishlist_items': wishlist_items})
+    return JsonResponse({'status': 'success', 'action': action, 'product_id': str(product_id)})
