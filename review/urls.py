fdcf882939e2f952519ecac77132a6ad7e4a6910
zufarra <zufarromli@gmail.com>
2024-10-27 17:08:54 +0700

    testing review again

--- review/urls.py
+++ review/urls.py
@@ -4,14 +4,10 @@ from review.views import edit_review, delete_review
 app_name = 'review'
 
 urlpatterns = [
-    path('<uuid:id>', show_review, name='show_review'),
-    path('create-review/<uuid:id>', create_review , name='create_review'),
-    path('edit-review/<uuid:id>', edit_review, name='edit_review'),
-    path('delete-review/<uuid:id>', delete_review, name='delete_review'),
-    path('json/', show_json, name='show_json'),
-
-
-
-
+    path('<uuid:id>/', show_review, name='show_review'),
+    path('create-review/<uuid:id>/', create_review , name='create_review'),
+    path('edit-review/<uuid:id>/', edit_review, name='edit_review'),
+    path('delete-review/<uuid:id>/', delete_review, name='delete_review'),
+    path('json/<uuid:product_id>/', show_json, name='show_json'),
 
 ]
