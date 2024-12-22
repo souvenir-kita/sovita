456bacc635230be0a546b0a652138409946833d9
reyhanardian16 <reyhanardian16@gmail.com>
2024-12-22 14:54:25 +0700

    config views & url wishlist

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,9 +1,13 @@
 import json
-from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404, render
 from django.contrib.auth.decorators import login_required
 from .models import Wishlist
 from adminview.models import Product
+from django.core import serializers
+from django.views.decorators.csrf import csrf_exempt
 
 @login_required
 def show_wishlist(request):
@@ -38,3 +42,76 @@ def toggle_wishlist(request, product_id):
         })
 
     return JsonResponse({'status': 'error', 'message': 'Invalid request method'}, status=400)
+
+@csrf_exempt
+def remove_wishlist_flutter(request, product_id):
+    user = request.user
+    if not request.user.is_authenticated:
+        return JsonResponse({'error': 'User not authenticated'}, status=401)
+
+    try:
+        if request.method == 'POST':
+            # user = request.user
+            product = Product.objects.get(pk=product_id)
+            
+            wishlist_item = Wishlist.objects.get(user=user, product=product)
+            wishlist_item.delete()
+            return JsonResponse({"status": "success"}, status=200)
+    except Product.DoesNotExist:
+            return JsonResponse({'error': 'Product not found'}, status=404)
+    except Exception as e:
+            return JsonResponse({'error': str(e)}, status=500)
+    return JsonResponse({'error': 'Invalid request method'}, status=400)
+
+    # try:
+    #     product = Product.objects.get(id=product_id)
+    # except Product.DoesNotExist:
+    #     return Response({'error': 'Product not found'}, status=404)
+
+    # wishlist_item = Wishlist.objects.filter(user=user, product=product).first()
+
+    # if wishlist_item:
+    #     wishlist_item.delete()
+    #     return Response({'message': 'Product removed from wishlist'}, status=200)
+    # else:
+    #     Wishlist.objects.create(user=user, product=product)
+    #     return Response({'message': 'Product added to wishlist'}, status=201)
+
+@csrf_exempt
+def add_wishlist_flutter(request, product_id):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        product =get_object_or_404(Product, product_id)
+        wishlist = Wishlist.objects.create(
+            user = request.user,
+            product = product.pk,
+            description = data["description"],
+            priority = int(data["priority"]),
+        )
+        wishlist.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
+    
+@csrf_exempt
+def update_wishlist_flutter(request):
+    if request.method == 'POST':  # Handle POST as update
+        try:
+            data = json.loads(request.body)
+            wishlist = Wishlist.objects.get(data["productId"])
+            wishlist.description = data["description"],
+            wishlist.priority = data["priority"],
+            wishlist.save()
+            return JsonResponse({'status': 'success', 'message': 'Wishlist updated successfully!'})
+        except Wishlist.DoesNotExist:
+            return JsonResponse({'error': 'Wishlist not found'}, status=404)
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=500)
+    return JsonResponse({'error': 'Invalid request method'}, status=400)
+
+@csrf_exempt
+def show_json(request):
+    data = Wishlist.objects.filter(user=request.user)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
